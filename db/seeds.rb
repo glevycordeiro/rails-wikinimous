d3d714abf379c4b373eeb854d4f31083c768129b
Goncalo Cordeiro <glevycordeiro@gmail.com>
2020-01-30 19:27:41 +0000

    added seeds with Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating 10 fake articles...'
+10.times do
+  article = Article.new(
+    title:    Faker::Book.title,
+    content: "#{Faker::Book.genre}"
+  )
+  article.save!
+end
+puts 'Finished!'
